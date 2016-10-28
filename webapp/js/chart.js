c4e53ab25ceb8f68cfa46769811a53c5a3f62e1c
Y. Biletskyi <yuriy.biletskyi@thenextview.nl>
2016-10-28 14:36:18 +0000

    chart

--- webapp/js/chart.js
+++ webapp/js/chart.js
@@ -1,16 +1,49 @@
-function createChart(){
+function createChart(container,receivedData){
+	// console.log(receivedData);
+	// Масив даних
+	var data = [
+	// 	{
+	//     date: "1:12",
+	//     close: "58.13"
+	// }, {
+	//     date: "1:16",
+	//     close: "53.98"
+	// }, {
+	//     date: "1:30",
+	//     close: "67.00"
+	// }, {
+	//     date: "1:40",
+	//     close: "89.70"
+	// }, {
+	//     date: "1:42",
+	//     close: "99.00"
+	// }
+	];
+	var iii;
+		for (iii = 0; iii < receivedData.length; iii++) {
+			data.push({date: receivedData[iii].CREATION_TS, close: receivedData[iii].TEMP});
+		}	
+	// var parseDate = d3.time.format("%H:%M").parse;
+	data.forEach(function (d) {
+	    // d.date = parseDate(d.date);
+	    d.date = new Date(+d.date.substring(6, 19));
+	    d.close = +d.close;
+	});
+	
+	
 	var margin = {
 	    top: 30,
 	    right: 20,
 	    bottom: 50,
 	    left: 50
 	};
-	var width = 600 - margin.left - margin.right;
-	var height = 270 - margin.top - margin.bottom;
-	
+	var width = $(container).width() - margin.left - margin.right;
+	var height =  $(container).height() - margin.top - margin.bottom;
+	console.log(width);
+	console.log(height);
 	//змініть тут на значення топ і нижньої меж
-	var bottomY = "20";
-	var topY = "80";
+	var bottomY = "4";
+	var topY = "20";
 	
 	//30 - це відступ справа і зліва лінії від початку графіка 
 	var x = d3.time.scale().range([30, width-30]);
@@ -22,7 +55,7 @@ function createChart(){
 	var yAxis = d3.svg.axis().scale(y)
 	    .orient("left").innerTickSize(-width)
 	    .outerTickSize(0)
-	    .ticks(7);
+	    .ticks(4);
 	
 	var valueline = d3.svg.line()
 	    .x(function (d) {
@@ -31,39 +64,17 @@ function createChart(){
 	    .y(function (d) {
 	      return y(d.close);
 	    });
-	
-	var svg = d3.select("body")
+	var classN = $(container).attr('id');
+	var svg = d3.select("#"+classN)
 	    .append("svg")
 	    .attr("width", width + margin.left + margin.right)
 	    .attr("height", height + margin.top + margin.bottom)
 	    .append("g")
 	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 	
-	// Масив даних
-	var data = [{
-	    date: "1:12",
-	    close: "58.13"
-	}, {
-	    date: "1:16",
-	    close: "53.98"
-	}, {
-	    date: "1:30",
-	    close: "67.00"
-	}, {
-	    date: "1:40",
-	    close: "89.70"
-	}, {
-	    date: "1:42",
-	    close: "99.00"
-	}];
+
 	
-	var parseDate = d3.time.format("%H:%M").parse;
-	data.forEach(function (d) {
-	    d.date = parseDate(d.date);
-	    d.close = +d.close;
-	});
-	
-	
+	console.log(data);
 	
 	// Scale the range of the data
 	x.domain(d3.extent(data, function (d) {
@@ -71,7 +82,8 @@ function createChart(){
 	    }));
 	y.domain([0, d3.max(data, function (d) {
 	//30 потрібно вже вам змінювати на значення взалежності від якого буде верхня межа графа
-	    return d.close+30;
+	    // return d.close+30;
+	    return 30;
 	    })]);
 	    
 	
@@ -103,24 +115,24 @@ function createChart(){
 	svg.append("text")      // text label for the x axis
 	         .attr("transform", "rotate(-90)")
 	         .attr("x", -(height/2) )
-	         .attr("y",  -(margin.left-10))
+	         .attr("y",  -(margin.left-20))
 	         .style("text-anchor", "middle")
 	         .text("Temperatuur & Max & Min");
 	          
-	svg.selectAll("dot").data(data)
-	   .enter().append("circle")
-	   .attr("cx", function(d) { return x(d.date); })
-	   .attr("cy", function(d) { return y(d.close); });
+	// svg.selectAll("dot").data(data)
+	//   .enter().append("circle")
+	//   .attr("cx", function(d) { return x(d.date); })
+	//   .attr("cy", function(d) { return y(d.close); });
 	   
-	svg.selectAll("dot").data(data)
-	   .enter().append("circle")
-	   .attr("cx", function(d) { return x(d.date); })
-	   .attr("cy", function(d) { return y(bottomY); })
-	   .attr("class", "bottom-line-dot");
+	// svg.selectAll("dot").data(data)
+	//   .enter().append("circle")
+	//   .attr("cx", function(d) { return x(d.date); })
+	//   .attr("cy", function(d) { return y(bottomY); })
+	//   .attr("class", "bottom-line-dot");
 	   
-	svg.selectAll("dot").data(data)
-	   .enter().append("circle")
-	   .attr("cx", function(d) { return x(d.date); })
-	   .attr("cy", function(d) { return y(topY); })
-	   .attr("class", "top-line-dot");
+	// svg.selectAll("dot").data(data)
+	//   .enter().append("circle")
+	//   .attr("cx", function(d) { return x(d.date); })
+	//   .attr("cy", function(d) { return y(topY); })
+	//   .attr("class", "top-line-dot");
 }
