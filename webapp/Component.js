6c37abde96e17796d9a71db1c9a639ae74806e7a
dAzzl3 <o.khomenko1993@gmail.com>
2016-10-31 09:00:28 +0000

    info

--- webapp/Component.js
+++ webapp/Component.js
@@ -22,9 +22,12 @@ sap.ui.define([
 
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
+			
 			var oCore = sap.ui.getCore();
 			console.log(this.getModel("odataModel"));
+			console.log(this.getModel("jsonModel"));
 			oCore.setModel(this.getModel("odataModel"), "odataModel");
+			oCore.setModel(this.getModel("jsonModel"), "jsonModel");
 			
 		
 		
--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/ws/WebSocket"
+], function(Controller, WebSocket) {
 	"use strict";
 
 	return Controller.extend("iotkid.controller.main", {
@@ -27,48 +28,154 @@ sap.ui.define([
 			// sap.ui.getCore().setModel(oModel);
 			// var m2 = sap.ui.getCore().getModel();
 			// this.getView().setModel(oModel);
-			
-	var uModel=new sap.ui.model.json.JSONModel();
-		// uModel.loadData( "/tnv/iot/services/gensense.xsodata/GenericMessages?$top=1000&$orderby=CREATION_TS desc", {}, false, "GET");
-		// uModel.loadData( "/tnv/iot/services/gensense.xsodata/GenericMessages"
-		// +"?$filter=((DEVICEID eq "++")  or (DEVICEID eq "+thisKpiId+") )"
-		uModel.loadData( "/tnv/iot/services/gensense.xsodata/GenericMessages"
-			+"?$filter=((DEVICEID eq '0059AC00001502BD')  or (DEVICEID eq '0059AC00001502B5') or (DEVICEID eq '0059AC00001502C2') )"
-			+"&$apply=groupby((DEVICEID,TEMP))"
-			+"&$top=50&$orderby=TEMP desc"
-			,{}, false, "GET");
-		console.log(uModel.getData().d.results);
-		var i;
-		for (i = 0; i < uModel.getData().d.results.length; i++) { 
-				console.log(uModel.getData().d.results[i].DEVICEID);
-		}
+
+			this.data = [{
+				DEVICEID: "0059AC00001502B5"
+			}, {
+				DEVICEID: "0059AC00001502C2"
+			}];
 
 		},
+
+		getData: function() {
+			var uModel = new sap.ui.model.json.JSONModel();
+			var jsonModel = sap.ui.getCore().getModel("jsonModel");
+
+			var jsonData = [];
+			var oResult;
+
+			for (var i = 0; i < this.data.length; i++) {
+
+				uModel.loadData("/tnv/iot/services/gensense.xsodata/GenericMessages" +
+					"?$filter=((DEVICEID eq '" + this.data[i].DEVICEID + "') )" +
+					"&$top=1&$orderby=CREATION_TS desc", {}, false, "GET");
+
+				oResult = uModel.getProperty("/d/results/0");
+				oResult.name = "Milkpak " + (i + 1);
+				this.adjustModel(oResult)
+
+				jsonData.push(oResult);
+
+			}
+			jsonModel.setProperty("/data", jsonData);
+			jsonModel.refresh(true);
+			console.log(jsonModel);
+			// uModel.loadData( "/tnv/iot/services/gensense.xsodata/GenericMessages?$top=1000&$orderby=CREATION_TS desc", {}, false, "GET");
+			// uModel.loadData( "/tnv/iot/services/gensense.xsodata/GenericMessages"
+			// +"?$filter=((DEVICEID eq "++")  or (DEVICEID eq "+thisKpiId+") )"
+			// uModel.loadData("/tnv/iot/services/gensense.xsodata/GenericMessages" +
+			// 	"?$filter=((DEVICEID eq 'DEVICEID')  or (DEVICEID eq '0059AC00001502B5') or (DEVICEID eq '0059AC00001502C2') )" +
+			// 	"&$apply=groupby((DEVICEID,TEMP))" + "&$top=50&$orderby=TEMP desc", {}, false, "GET");
+			// console.log(uModel.getData().d.results);
+			// var i;
+			// for (i = 0; i < uModel.getData().d.results.length; i++) {
+			// 	console.log(uModel.getData().d.results[i].DEVICEID);
+			// }
+
+		},
+
+		adjustModel: function(oResult) {
+			 oResult.TEMP = Math.round(oResult.TEMP);
+			// oResult.TEMP = new Date().getMilliseconds();
+
+			var bHot = (oResult.TEMP >= 4 && oResult.TEMP <= 10);
+
+			oResult.infoState = bHot ? "Success" : "Error";
+			oResult.info = bHot ? "juiste temperatuur" : "te warm";
+
+		},
+
+		changeData: function(DEVICEID, TEMP) {
+			var that = this;
+			var aData = sap.ui.getCore().getModel("jsonModel").getData();
+			aData.forEach(function(item) {
+				if (item.DEVICEID === DEVICEID) {
+					item.TEMP = TEMP;
+					that.adjustModel(item);
+				}
+			});
+		},
+
+		openConnection: function(oEvent) {
+			// var inputBox = this.getView().byId("wsInput");
+			// var sUrl = inputBox.getValue();
+			var sUrl = "wss://www.italks.eu/app?id=BE7A005E&token=7B7lwnfs3m6rRJrfiGkKjA";
+			this.webSocket = new WebSocket(sUrl).attachMessage(this.webSocketMessageRecieved, this).attachError(this.webSocketError,
+				this).attachOpen(this.webSocketOpened, this);
+		},
+
+		webSocketOpened: function(oEvent) {
+			// this.getView().byId("page").setTitle("Connected");
+			// this.webSocket.send({
+			// 	cmd: 'cq'
+			// });
+		},
+
+		webSocketMessageRecieved: function(oEvent) {
+			var dataString = oEvent.getParameters().data;
+			var data = JSON.parse(dataString);
+			console.log(data);
+			// this.changeData (data.EUI, data.TEMP);
+			// if (data.cmd === "rx") {
+			// 	data.timestamp = new Date();
+			// 	console.log(data);
+			// 	var jsonModel = this.getView().getModel("jsonModel");
+			// 	var jsonData = jsonModel.getProperty("/data");
+			// 	jsonData.push(data);
+			// 	jsonModel.setProperty("/data", jsonData);
+			// 	if (data.data.startsWith("01")) {
+			// 		//http://1m2m.eu/services/GETPAYLOAD?Human=0&PL=0102096100064f7a3c07a50300000000
+			// 		var locationModel = this.getView().getModel("locationModel");
+			// 		locationModel.setProperty("/data", data);
+			// 		var sUrl = "/destinations/1m2mpayload/services/GETPAYLOAD?Human=0&PL=" + data.data;
+			// 		locationModel.loadData(sUrl, null, true, "GET", true);
+			// 	} else {
+			// 		var messageModel = this.getView().getModel("messageModel");
+			// 		messageModel.setProperty("/data", data);
+			// 		var sUrl = "/destinations/1m2mpayload/services/GETPAYLOAD?Human=0&PL=" + data.data;
+			// 		messageModel.loadData(sUrl, null, true, "GET", true);
+			// 	}
+			// }
+		},
+
+		webSocketError: function(oEvent) {
+			console.error(oEvent);
+		},
+
 		onPress: function(oEvent) {
-			
-		var sPath = oEvent.getSource().getBindingContext("odataModel").getPath();
-		var sId = "overviewpage";
-	
-		var oApp = sap.ui.getCore().byId("__xmlview0--appId");
-		
-		var overviewPage = oApp.getPage(sId);
-		
-			if(overviewPage) {
-				overviewPage.data("sPath",sPath);
+
+			var sPath = oEvent.getSource().getBindingContext("jsonModel").getPath();
+			var sId = "overviewpage";
+
+			var oApp = sap.ui.getCore().byId("__xmlview0--appId");
+
+			var overviewPage = oApp.getPage(sId);
+
+			if (overviewPage) {
+				overviewPage.data("sPath", sPath);
 				oApp.to(overviewPage);
-			}else{
-			overviewPage = sap.ui.view({
+			} else {
+				overviewPage = sap.ui.view({
 					id: sId,
 					viewName: "iotkid.view.overviewPage",
 					type: sap.ui.core.mvc.ViewType.XML
-				});	
-				overviewPage.data("sPath",sPath);
+				});
+				overviewPage.data("sPath", sPath);
 				oApp.addPage(overviewPage);
 				oApp.to(overviewPage);
 			}
 
-		
-			
+		},
+		onBeforeRendering: function() {
+			var that = this;
+			this.getData();
+				setInterval(function (){
+					that.getData();
+				}, 10000)
+
+		},
+		onAfterRendering: function() {
+			this.openConnection();
 		}
 
 	});
